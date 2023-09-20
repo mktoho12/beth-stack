7758dad95f04554c880cbc761d1f1de4fc574f53
mktoho <mk@mktoho.org>
2023-09-21 07:06:49 +0900

    add Footer

--- src/index.tsx
+++ src/index.tsx
@@ -24,30 +24,10 @@ const app = new Elysia()
     const data = await db.select().from(todos).all()
     return (
       <>
-        <main class="text-3xl leading-normal">
+        <main class="px-4 text-xl md:text-3xl leading-normal">
           <h1 class="text-4xl font-bold text-center">Todo List</h1>
           <TodoList todos={data} />
-          <footer class="mt-8">
-            <p class="text-base text-right">
-              written by <a href="https://elysiajs.com">ElysiaJS</a>,{' '}
-              <a href="https://htmx.org">htmx</a> and{' '}
-              <a href="https://orm.drizzle.team">Drizzle ORM</a>. running on{' '}
-              <a href="https://bun.sh">Bun</a>. Data Persistence by{' '}
-              <a href="https://turso.tech">Turso</a>.
-            </p>
-            <div class="mt-2">
-              <p class="text-base">I made it while watching this video.</p>
-              <iframe
-                width="400"
-                height="225"
-                src="https://www.youtube.com/embed/cpzowDDJj24?si=hIHzjRMMPIDPYTOt"
-                title="YouTube video player"
-                frame-border="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                allowfullscreen
-              ></iframe>
-            </div>
-          </footer>
+          <Footer />
         </main>
       </>
     )
@@ -138,7 +118,7 @@ const TodoItem = ({ content, completed, id }: Todo) => (
     <p class="grow">{content}</p>
     <input
       type="checkbox"
-      class="w-8"
+      class="w-6 md:w-8"
       checked={completed}
       hx-post={`/todos/toggle/${id}`}
       hx-target="closest div"
@@ -173,4 +153,44 @@ const TodoForm = () => (
     <input type="text" name="content" class="border border-black px-1" />
     <button type="submit">Add</button>
   </form>
+)
+
+const Footer = () => (
+  <footer class="mt-8">
+    <p class="text-base text-right">
+      written by <a href="https://elysiajs.com">ElysiaJS</a>,{' '}
+      <a href="https://htmx.org">htmx</a> and{' '}
+      <a href="https://orm.drizzle.team">Drizzle ORM</a>. running on{' '}
+      <a href="https://bun.sh">Bun</a>. Data Persistence by{' '}
+      <a href="https://turso.tech">Turso</a>.
+    </p>
+    <div class="mt-2">
+      <p class="text-base">I made it while watching this video.</p>
+      <iframe
+        src="https://www.youtube.com/embed/cpzowDDJj24?si=hIHzjRMMPIDPYTOt"
+        title="YouTube video player"
+        frame-border="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+        class="w-full md:w-[400px] aspect-video"
+      ></iframe>
+    </div>
+    <div class="absolute right-4 bottom-4">
+      <a href="https://github.com/mktoho12/beth-stack">
+        <svg
+          width="32"
+          viewBox="0 0 98 96"
+          class="aspect-[98/96]"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+            d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z"
+            fill="#24292f"
+          />
+        </svg>
+      </a>
+    </div>
+  </footer>
 )
